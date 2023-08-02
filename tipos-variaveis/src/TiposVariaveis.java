15936c0d5dbae4d5c4f52e78e0181f9a6244d7b6
bruno <brunogeraldosilva46@gmail.com>
2023-08-02 20:01:52 -0300

    Alterações

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,22 @@
-public class App {
+public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        //Tipos primitivos
+        //Estudem a classe String que representa texto na aplicação
+
+        String meuNome = "Bruno Geraldo";
+
+        double salarioMinimo = 2500;
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+
+        int numero = 5;
+
+        numero = 10;
+
+        System.out.print(numero);
+        final double VALORDEPI = 3.14;
+
     }
 }
